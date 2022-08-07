3dd1ec5cb468b8c1cfecb973c3293d3c3067f5b9
Issa71 <mohamedissa95@gmail.com>
2022-08-07 23:21:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+document.querySelector("body > section > form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    console.log("THIS:", this);
+
+    const body = {
+        name: event.target.name.value,
+        habitat: event.target.habitat.value,
+        age: event.target.age.value,
+        gender: event.target.gender.value};
+
+    console.log("BODY:", body);
+
+    axios.post("http://localhost:8080/duck/create", body)
+        .then(response => {
+            console.log(response);
+            event.target.reset();
+            event.target.name.focus();
+        })
+        .catch(err => console.error(err));});
+
+function getAllDucks() {
+
+    axios
+    .get("http://localhost:8080/duck/readAll")
+    .then(response => {
+        console.log("ducks: ", response.data);
+    })
+    .catch(err => console.error(err));};
+
+
+function getDuckByID(id) {
+    axios
+    .get("http://localhost:8080/duck/readById/" + id)
+    .then(response => {
+    console.log("duck: ", response.data);
+    })
+    .catch(err => console.error(err));};
+
+function delDuckbyID(id) {
+axios
+.delete("http://localhost:8080/duck/delete/" + id)
+.then(response => {
+console.log("duck: ", response.data);
+})
+.catch(err => console.error(err));};
+
